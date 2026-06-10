7558ff9bb3a2f62d92bc8734325ff88dfeb143c7
gabriela_h_dias <gabriela_h_dias@estudante.sesisenai.org.br>
2026-06-10 10:11:39 -0300

    add

--- index.php
+++ index.php
@@ -27,6 +27,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+        <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
     <h1>Sistema de Login Simples</h1>
